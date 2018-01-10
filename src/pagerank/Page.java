2a2f403770f82180743c4b3770a9be7f7dd5afa4
camheras <camheras@gmail.com>
2018-01-10 10:04:47 +0100

    ajout class Page

---
+++ src/pagerank/Page.java
@@ -0,0 +1,15 @@
+package pagerank;
+
+/**
+ * Created by i161040 on 10/01/2018.
+ */
+public class Page {
+    float importance;
+    int id;
+
+    public Page(float importance, int id) {
+        this.importance = importance;
+        this.id = id;
+    }
+
+}
