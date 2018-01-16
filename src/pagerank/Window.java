1ad6794e137b7ad22769285427a15b6c31400026
maxime <pocard.maxime@gmail.com>
2018-01-16 19:30:55 +0000

    Full screen with great proportions

--- src/pagerank/Window.java
+++ src/pagerank/Window.java
@@ -18,7 +18,8 @@ import java.util.List;
 
 public class Window {
     private static final Color DEFAULT_BG_COLOR = Color.decode("#FAFBFF");
-    private static final Dimension DEFAULT_SIZE = new Dimension(500,500);
+    private static final Dimension WINDOW_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
+    private static final Dimension DEFAULT_SIZE = new Dimension(WINDOW_SIZE.width-100, WINDOW_SIZE.height);
     //
     private JGraphModelAdapter m_jgAdapter;
 
@@ -27,7 +28,7 @@ public class Window {
         Collections.reverse(pages);
 
         JFrame window = new JFrame("PageRankTD");
-        window.setSize(DEFAULT_SIZE);
+        window.setSize(WINDOW_SIZE);
 
         // create a JGraphT graph
         ListenableGraph g = new ListenableDirectedGraph(DefaultEdge.class);
@@ -45,17 +46,6 @@ public class Window {
         window.add(jgraph);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
-
-//        List<Page> pages = new ArrayList<>();
-//        pages.add(new Page(89,1));
-//        pages.add(new Page(70,2));
-//        pages.add(new Page(70,3));
-//        pages.add(new Page(70,4));
-//        pages.add(new Page(63,5));
-//        pages.add(new Page(52,6));
-//        pages.add(new Page(42,7));
-//        pages.add(new Page(40,8));
-//        pages.add(new Page(40,9));
 
         //Previous page added to the graph
         Page previous = null;
@@ -123,68 +113,4 @@ public class Window {
         }
         return nb;
     }
-
-    public void old(){
-        //Fenetre principale
-        JFrame window = new JFrame("PageRankTD");
-        window.setSize(400,400);
-        //Contenu de la fenetre
-        JPanel pan = new JPanel();
-        //Layout principal
-        pan.setLayout(new GridLayout(3,1));
-
-        //Header
-        Panel headerP = new Panel();
-        BorderLayout headerL = new BorderLayout();
-        headerP.setLayout(headerL);
-        //Search
-        Panel searchP = new Panel();
-        searchP.setMaximumSize(new Dimension(50,50));
-        GridLayout searchL = new GridLayout(1,2);
-        searchP.setLayout(searchL);
-        searchP.setPreferredSize(new Dimension(window.getWidth(), 75));
-        //Results
-        JScrollPane resultP = new JScrollPane();
-        ScrollPaneLayout resultL = new ScrollPaneLayout();
-        resultP.setLayout(resultL);
-
-        /* Header elements */
-        JTextArea text = new JTextArea("Welcome to our PageRank !");
-        text.setEditable(false);
-        headerP.add(text);
-
-        /* Search elements */
-        JTextField search = new JTextField("");
-        JButton send = new JButton("Send");
-//        send.setSize(30,30);
-//        pan.setSize(400,30);
-        searchP.add(search);
-        searchP.add(send);
-
-
-        /* Results elements */
-        DefaultListModel<String> l1 = new DefaultListModel<>();//Empty list
-        JList<String> list = new JList<>(l1);
-        list.setBounds(0,0, window.getWidth(),200);
-        resultP.add(list);
-
-        send.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                l1.addElement(search.getText());
-                System.out.println(search.getText());
-            }
-        });
-
-
-        /* Add each layout to the main one */
-        pan.add(headerP,BorderLayout.NORTH);
-        pan.add(searchP,BorderLayout.CENTER);
-        pan.add(resultP,BorderLayout.SOUTH);
-
-        window.add(pan);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
-    }
-
 }
